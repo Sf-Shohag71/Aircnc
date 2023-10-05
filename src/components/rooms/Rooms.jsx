3e08f456002d6f6cc93e91f788a1b4fcd7455edd
Sf-Shohag71 <shakhfarid71@gmail.com>
2023-10-05 13:53:07 +0600

    Add custom loader

--- src/components/rooms/Rooms.jsx
+++ src/components/rooms/Rooms.jsx
@@ -2,15 +2,26 @@ import React, { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import Container from "../shared/Container/Container";
 import Card from "./Card";
+import Loader from "../shared/Loader";
 
 const Rooms = () => {
   const [rooms, setRooms] = useState([]);
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
+    setLoading(true);
     fetch("./rooms.json")
       .then((res) => res.json())
-      .then((data) => setRooms(data))
+      .then((data) => {
+        setRooms(data);
+        setLoading(false);
+      })
       .catch((err) => toast.error(err.message));
   }, []);
+
+  if (loading) {
+    return <Loader></Loader>;
+  }
+
   return (
     <Container>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-5 gap-8">
