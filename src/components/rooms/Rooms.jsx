05ea1731e810fb8e09471b8976921cfced85ed51
Sf-Shohag71 <shakhfarid71@gmail.com>
2023-10-06 11:09:19 +0600

    Rooms showed by clicking category

--- src/components/rooms/Rooms.jsx
+++ src/components/rooms/Rooms.jsx
@@ -3,20 +3,32 @@ import toast from "react-hot-toast";
 import Container from "../shared/Container/Container";
 import Card from "./Card";
 import Loader from "../shared/Loader";
+import { useSearchParams } from "react-router-dom";
+import Heading from "../Heading/Heading";
 
 const Rooms = () => {
   const [rooms, setRooms] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [params, setParams] = useSearchParams();
+  const category = params.get("category");
+
   useEffect(() => {
     setLoading(true);
     fetch("./rooms.json")
       .then((res) => res.json())
       .then((data) => {
-        setRooms(data);
+        if (category) {
+          const filteredData = data.filter(
+            (room) => room.category === category
+          );
+          setRooms(filteredData);
+        } else {
+          setRooms(data);
+        }
         setLoading(false);
       })
       .catch((err) => toast.error(err.message));
-  }, []);
+  }, [category]);
 
   if (loading) {
     return <Loader></Loader>;
@@ -24,11 +36,21 @@ const Rooms = () => {
 
   return (
     <Container>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-5 gap-8">
-        {rooms.map((room) => (
-          <Card></Card>
-        ))}
-      </div>
+      {rooms && rooms.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-5 gap-8">
+          {rooms.map((room, index) => (
+            <Card key={index} room={room}></Card>
+          ))}
+        </div>
+      ) : (
+        <div className="pt-12">
+          <Heading
+            title="No rooms available in this category"
+            subtitle="Please select other category"
+            center={true}
+          ></Heading>
+        </div>
+      )}
     </Container>
   );
 };
