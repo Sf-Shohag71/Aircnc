b968bdbd9be75e7a96fd2450efdbb3e92727639b
Sf-Shohag71 <shakhfarid71@gmail.com>
2023-10-07 09:20:45 +0600

    Completed inital frontend design

--- src/pages/RoomDetails/RoomDetails.jsx
+++ src/pages/RoomDetails/RoomDetails.jsx
@@ -12,7 +12,6 @@ const RoomDetails = () => {
           <Header></Header>
           <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6">
             <RoomInfo></RoomInfo>
-
             <div className="mb-10 md:col-span-3 order-first md:order-last">
               <RoomReservation></RoomReservation>
             </div>
