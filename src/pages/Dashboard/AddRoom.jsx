efa1856a2376cf39f1a9559678556c61da7d4055
Sf-Shohag71 <shakhfarid71@gmail.com>
2023-10-27 21:04:46 +0600

    grab the add room form data and collect start and end date using calender

---
+++ src/pages/Dashboard/AddRoom.jsx
@@ -0,0 +1,82 @@
+import React, { useContext, useState } from "react";
+import AddRoomForm from "../../components/Forms/AddRoomForm.jsx";
+import { imageUpload } from "../../api/imageUpload.js";
+import { AuthContext } from "../../providers/AuthProvider.jsx";
+
+const AddRoom = () => {
+  const [loading, setLoading] = useState(false);
+  const [uploadButtonText, setUploadButtonText] = useState("Upload Image");
+  const { user } = useContext(AuthContext);
+  const [dates, setDates] = useState({
+    startDate: new Date(),
+    endDate: new Date(),
+    key: "selection",
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setLoading(true);
+    const location = event.target.location.value;
+    const category = event.target.category.value;
+    const title = event.target.title.value;
+    const price = event.target.price.value;
+    const total_guest = event.target.total_guest.value;
+    const bedrooms = event.target.bedrooms.value;
+    const bathrooms = event.target.bathrooms.value;
+    const description = event.target.description.value;
+    const from = dates.startDate;
+    const to = dates.endDate;
+    const image = event.target.image.files[0];
+
+    // Upload image
+    imageUpload(image)
+      .then((data) => {
+        const roomData = {
+          image: data.data.display_url,
+          location,
+          title,
+          category,
+          price: parseFloat(price),
+          total_guest,
+          bedrooms,
+          bathrooms,
+          description,
+          from,
+          to,
+          host: {
+            name: user?.displayName,
+            photo: user?.photoURL,
+            email: user?.email,
+          },
+        };
+        console.log(roomData);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err.message);
+        setLoading(false);
+      });
+  };
+
+  const handleImageChange = (image) => {
+    setUploadButtonText(image.name);
+  };
+  const handleDates = (ranges) =>{
+    setDates(ranges.selection);
+  }
+
+  return (
+    <>
+      <AddRoomForm
+        handleSubmit={handleSubmit}
+        loading={loading}
+        handleImageChange={handleImageChange}
+        uploadButtonText={uploadButtonText}
+        dates={dates}
+        handleDates={handleDates}
+      ></AddRoomForm>
+    </>
+  );
+};
+
+export default AddRoom;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -6,6 +6,7 @@ import SignUp from "../pages/SignUp/SignUp";
 import RoomDetails from "../pages/RoomDetails/RoomDetails";
 import PrivateRoute from "./PrivateRoute";
 import DashboardLayout from "../layouts/DashboardLayout";
+import AddRoom from "../pages/Dashboard/AddRoom";
 
 export const router = createBrowserRouter([
   {
@@ -36,6 +37,12 @@ export const router = createBrowserRouter([
   },
   {
     path: "dashboard",
-    element: <DashboardLayout></DashboardLayout>
+    element: <DashboardLayout></DashboardLayout>,
+    children:[
+      {
+        path: "add-room",
+        element: <AddRoom></AddRoom>
+      },
+    ]
   },
 ]);
